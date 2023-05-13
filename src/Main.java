ef26b00442bb772397c37685431dbabd02224956
Andrei <andrei.cojocariu@icloud.com>
2023-05-13 20:25:35 +0200

    v0.1

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,26 @@ import java.util.random.RandomGenerator;
 
 public class Main {
 
-
     public static void main(String[] args) {
         QQWing_Sudoku newSudoku = new QQWing_Sudoku();
 
         int[] Sudoku = newSudoku.generateSudoku(1);
 
-//        int[] Sudoku = {0, 0, 4, 3, 0, 0, 2, 7, 6, 0, 1, 0, 0, 6, 2, 4, 0, 8, 7, 2, 6, 0, 4, 0, 0, 1, 0, 0, 9, 0, 4, 7, 5, 0, 0, 3, 0, 7, 3, 1, 0, 6, 9, 0, 0, 0, 6, 0, 2, 3, 0, 0, 4, 1, 6, 8, 0, 0, 2, 7, 0, 3, 0, 9, 0, 0, 8, 1, 4, 5, 0, 0, 1, 4, 2, 0, 0, 3, 8, 0, 0};
-
-        List <Integer> first =  new ArrayList<>();
+        List <Integer> first;
 
         first =  newSudoku.population(Sudoku);
 
         List<Integer>posCross = new ArrayList<>();
 
-        for (int i = first.size()-9; i < first.size(); i++) {
+        for (int i = first.size()-9; i < first.size(); i++)
             posCross.add(first.get(i));
-        }
 
 
-        for (int i=0 ; i<9 ;i++){
+        for (int i=0 ; i<9 ;i++)
             first.remove(first.size()-1);
-        }
 
-        List<List<Integer>> populations = new ArrayList<>();
+
+        List<List<Integer>> populations;
 
         populations = newSudoku.generatePopulation(first,posCross);// generate the population
 
@@ -39,37 +35,35 @@ public class Main {
         newSudoku.fitnessFunction(sudoku);
 
 
-
-
-        List<List<Integer>> population2 = new ArrayList<>();
-        List<Integer> parent1 = new ArrayList<>();
-        List<Integer> parent2 = new ArrayList<>();
-        List<Integer> child = new ArrayList<>();
+        List<List<Integer>> population2;
+        List<Integer> parent1;
+        List<Integer> parent2;
+        List<Integer> child;
         int bestIndividual = 0;
         int max = 0;
         double weight1 = 1;
         int last = 0;
         while (bestIndividual < 161) {
 
-
-             population2 = new ArrayList<>();
+            population2 = new ArrayList<>();
             Collections.shuffle(populations);
-            List<Integer> weights = new ArrayList<>();
+            List<Integer> weights;
             weights = newSudoku.weightedBy(populations,Sudoku);
 
             List<Integer> copilulMinune = new ArrayList<>();
-
 
             int max1 = 0;
             int max2 = 0;
             int index2 = 0;
             int index1 = 0;
+
             for(int i = 0; i < weights.size() ;i++) {
                 if (max1 < weights.get(i)) {
                     max1 = weights.get(i);
                     index1 = i;
                 }
             }
+
             for(int i = 0; i < weights.size() ;i++){
                 if(max2 < weights.get(i) && index1 != i){
                     max2 = weights.get(i);
@@ -77,27 +71,21 @@ public class Main {
                 }
             }
 
-//            System.out.println(index1);
-//            System.out.println(index2);
-
-
-
             for (int i = 0; i < populations.size(); i++) {
-//                parent1 = newSudoku.weithed_random_choices(populations,weights);
-//                parent2 = newSudoku.weithed_random_choices(populations,weights);
 
                 parent1 = populations.get(index1);
                 parent2 = populations.get(index2);
 
                 child = newSudoku.reproduce(parent1,parent2,posCross);
-                if(Math.random() > Math.pow((double)max/162,10)/weight1){//0.8 - 4 gauri vere
+
+                if(Math.random() > Math.pow((double)max/162,10)/weight1)
                     child = newSudoku.mutation(child,posCross);
-                }
+
                 population2.add(child);
-                if(weights.get(i) >= max){
+
+                if(weights.get(i) >= max)
                     copilulMinune = child;
 
-                }
             }
 
             for(int i = 0;i< populations.size();i++){
@@ -105,36 +93,22 @@ public class Main {
             }
 
 
-//            System.out.println("Populatia2");
-//            int index1 = 0;
-//            for(List<Integer>people : population2){
-//
-//                for (int i: people){
-//                    System.out.printf(i+" ");
-//                }
-//                System.out.printf(" - "+ weights.get(index1));
-//                index1++;
-//                System.out.println();
-//            }
-
 
             max = 0;
-//            System.out.println("weight: "+ weights.size());
+
             for (Integer weight : weights) {
-//                System.out.printf(weight+" ");
-                if (max < weight) {
-//                    System.out.printf(weight+" ");
+                if (max < weight)
                     max = weight;
-                }
             }
+
             bestIndividual = max;
-            if(max == last){
+            if(max == last)
                 weight1 += 0.01;
-            }
             else{
                 weight1 = 1;
                 last = max;
             }
+
             if(bestIndividual > 0) {
                 double res = Math.pow((double) max / 162, 10) / weight1;
                 System.out.println("Best fitness score: " + max + " " + res);
--- src/QQWing_Sudoku.java
+++ src/QQWing_Sudoku.java
@@ -9,28 +9,9 @@ public class QQWing_Sudoku {
 
     List<Integer> parent1 = new ArrayList<>();
     List<Integer> parent2 = new ArrayList<>();
-    public int[] vecFrec = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     public List<Integer> chromosome = new ArrayList<>();
     public List<Integer> pointCross = new ArrayList<>();
 
-
-
-    //    public List<Integer> pupulation(int[] Sudoku){
-//        for(int i = 0; i < 81;i+=9){
-//            int[] vecFrec = {0,0,0,0,0,0,0,0,0,0};
-//            for(int j = i; j < i+9;j++){ // iterate on the line
-//                if(Sudoku[i] != 0)
-//                    vecFrec[j]++;
-//            }
-//            for (int j = 1; j <= 9; j++) {
-//                if(vecFrec[j] == 0)
-//                    chromosome.add(j);
-//            }
-////            pointCross.add(chromosome.size());
-//
-//        }
-//        return chromosome;
-//    }
     public int[] generateSudoku(int dif) {
         int[] puzzle = new int[0];
         switch (dif) {
@@ -57,12 +38,7 @@ public class QQWing_Sudoku {
                 //Puzzle with difficulty SIMPLE as assessed by QQWing.
                 //Should require just minimal tuning of the parameters of the genetic algorithm
                 puzzle = ExampleQQWing.computePuzzleByDifficulty(Difficulty.SIMPLE);
-//                for(int i = 0; i < 9; i++){
-//                    for (int j = 0; j < 9; j++) {
-//                        System.out.print(puzzle[i]);
-//                    }
-//                    System.out.println();
-//                }
+
                 break;
             case 3:
                 //Puzzle with difficulty EASY as assessed by QQWing.
@@ -118,12 +94,9 @@ public class QQWing_Sudoku {
             f = 0;
             l = posCross.get(index);
             for (int k = 0; k < posCross.size(); k++) {
-
-//                System.out.printf("|"+f+ " "+l+"|   ");
-                for (int j = f; j < l; j++) {
-
+                for (int j = f; j < l; j++)
                     newPop.add(first.get(j));
-                }
+
 
                 Collections.shuffle(newPop);
                 newPopFinal.addAll(newPop);
@@ -132,7 +105,6 @@ public class QQWing_Sudoku {
                 index++;
                 if (index < 9)
                     l = posCross.get(index);
-
             }
             populations.add(newPopFinal);
         }
@@ -151,11 +123,6 @@ public class QQWing_Sudoku {
                 sudokuFull.add(sudokuGame[i]);
             }
         }
-
-//            for (Integer i : sudokuFull){
-//                System.out.printf(i+" ");
-//            }
-
         return sudokuFull;
     }
 
@@ -175,16 +142,16 @@ public class QQWing_Sudoku {
             frecVect = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         }
         int i1 = 0;
-        int i2 = 3;////verif
+        int i2 = 3;
         int j1 = 0;
         int j2 = 3;
+
         frecVect = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-//        System.out.println("sudokuFullPractic");
+
         for (int z = 0; z < 9; z++) {
             for (int i = i1; i < i2; i++) {
                 for (int j = j1; j < j2; j++) {
                     frecVect[sudokuFull.get(j * 9 + i)]++;
-//                    System.out.printf(j*9+i+" ");
                 }
             }
             for (int j = 1; j <= 9; j++) {
@@ -199,114 +166,66 @@ public class QQWing_Sudoku {
                 i2 += 3;
             }
             if ((z + 1) % 3 == 0) {
-
                 j1 += 3;
                 j2 += 3;
                 i1 = 0;
                 i2 = 3;
             }
         }
-//        System.out.println("suma = " + sumFinal);
         return sumFinal;
-    }
-
-
-    public List<List<Integer>> crossover(List<List<Integer>> population, List<Integer> posCross) {//out children
-        Collections.shuffle(population);
-        List<List<Integer>> newPopulation = new ArrayList<>();
-        List<Integer> people1 = new ArrayList<>();
-        List<Integer> people2 = new ArrayList<>();
-
-        int randomIndex;
-        int div;
-        for (int i = 0; i < population.size(); i += 2) {
-            randomIndex = (int) (Math.random() * (posCross.size() - 1));
-            div = posCross.get(randomIndex);
-            for (int j = 0; j < div; j++) {
-                people1.add(population.get(i).get(j));
-                people2.add(population.get(i + 1).get(j));
-            }
-            for (int j = div; j < population.get(i).size(); j++) {
-                people1.add(population.get(i + 1).get(j));
-                people2.add(population.get(i).get(j));
-            }
-            newPopulation.add(people1);
-            newPopulation.add(people2);
-
-
-        }
-        return newPopulation;
     }
 
     public List<Integer> mutation(List<Integer> child, List<Integer> posCross) {
         int randIndex = (int) (Math.random() * (posCross.size() ));
-//        System.out.printf("child1: "+child+" ");
-        int randFirst =0;
+
+        int randFirst = 0;
         int randSecond = 0;
         if (randIndex == 0) {
 
-            int infLim = 0;//[0,]
+            int infLim = 0;
             int supLim = posCross.get(randIndex);
 
-            randFirst = (int) (Math.random() * (supLim - infLim ) + infLim);///mod1
+            randFirst = (int) (Math.random() * (supLim - infLim ) + infLim);
             randSecond = (int) (Math.random() * (supLim - infLim) + infLim);
-            while(randFirst == randSecond){
+
+            while(randFirst == randSecond)
                 randSecond = (int) (Math.random() * (supLim - infLim) + infLim);
-            }
+
+
             int aux = child.get(randFirst);
             child.set(randFirst, child.get(randSecond));
             child.set(randSecond, aux);
-//            System.out.println("->"+randFirst+" "+ randSecond+ " cu 0");
-
 
         } else {
             int supLim = posCross.get(randIndex);
             int infLim = posCross.get(randIndex - 1);
+
             randFirst = (int) (Math.random() * (supLim - infLim ) + infLim);///mod1
             randSecond = (int) (Math.random() * (supLim - infLim) + infLim);
-            while(randFirst == randSecond){
+
+            while(randFirst == randSecond)
                 randSecond = (int) (Math.random() * (supLim - infLim) + infLim);
-            }
+
 
             int aux = child.get(randFirst);
             child.set(randFirst, child.get(randSecond));
             child.set(randSecond, aux);
 
-//            System.out.println("->"+randFirst+" "+ randSecond);
         }
-//        System.out.printf("child2: "+child);
-//        System.out.println("->"+randFirst+" "+ randSecond);
         return child;
     }
 
     public List<Integer> weightedBy(List<List<Integer>> population, int[] sudoku) {
+
         QQWing_Sudoku people = new QQWing_Sudoku();
         List<Integer> weights = new ArrayList<>();
-        for (List<Integer> people1 : population) {
+
+        for (List<Integer> people1 : population)
             weights.add(people.fitnessFunction(people.reconstructFunction(people1, sudoku)));
-        }
+
         return weights;
     }
 
-    public List<Integer> weithed_random_choices(List<List<Integer>> population, List<Integer> weights) {
-        int sum = 0;
-
-        for (int i : weights) {
-            sum += i;
-        }
-        int averageWeight = sum / weights.size();
-        int counter = 0;
-        Collections.shuffle(population);
-
-        for (int i = 0; i < population.size(); i++) {
-            if (weights.get(i) > averageWeight) {
-                return population.get(i);
-            }
-
-        }
-
-        return population.get(0);
-    }
 
     public List<Integer> reproduce (List<Integer> parent1,List<Integer> parent2,List<Integer>posCross){
         List<Integer> child = new ArrayList<>();
